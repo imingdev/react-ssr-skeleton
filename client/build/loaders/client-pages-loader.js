d8ba4fdbac0c0e1e55a9c96a7031c81cc611e403
mingdev <mingdev@163.com>
2020-09-29 14:53:00 +0800

    项目初始化

--- client/build/loaders/client-pages-loader.js
+++ client/build/loaders/client-pages-loader.js
@@ -1,18 +1,30 @@
 /**
  * @intro: page-loader.
  */
-const {formatFilePath} = require('../utils');
+const path = require('path');
+const { formatFilePath } = require('../utils');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isDevelopment = NODE_ENV === 'development';
+const resolve = (dir) => path.join(__dirname, '..', dir);
 
 module.exports = function () {
-  const {resourcePath} = this;
-  const formatResPath = formatFilePath(resourcePath);
+  const { resourcePath } = this;
+  const currentComponentPath = formatFilePath(resourcePath);
+  const mainComponentPath = formatFilePath(resolve('../src/main'));
+  const appComponentPath = formatFilePath(resolve('../src/pages/_app'));
+
+  let hotAcceptCode = '';
+  if (isDevelopment) hotAcceptCode = ' if (module.hot) module.hot.accept();';
 
   return `
-    import AppMain from '@/main';
-    import AppPage from '${formatResPath}';
+    import App from '${appComponentPath}';
+    import AppMain from '${mainComponentPath}';
+    import AppPage from '${currentComponentPath}';
 
     setTimeout(function () {
-      AppMain(AppPage);
+      AppMain(App, AppPage);
     }, 0);
-  `
+    ${hotAcceptCode}
+  `;
 };
